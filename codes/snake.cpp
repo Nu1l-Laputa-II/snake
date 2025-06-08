c3064acd4760313cbf93f238a5bf4d1d7bbdf9a7
Nu1L <Nu1l-Laputa@outlook.com>
2025-06-09 03:10:15 +0800

    FIN

--- codes/snake.cpp
+++ codes/snake.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <queue>
 
-const std::string banner = R"(
+using namespace std;
+
+const string banner = R"(
 -------------------------------------------------------
  ______     __   __     ______     __  __     ______    
 /\  ___\   /\ "-.\ \   /\  __ \   /\ \/ /    /\  ___\   
@@ -11,41 +14,64 @@ const std::string banner = R"(
   \/_____/   \/_/ \/_/   \/_/\/_/   \/_/\/_/   \/_____/
 -------------------------------------------------------)";
 
-const std::string welcomeMessage = R"(
+const string welcomeMessage = R"(
 Welcome to the Snake Game!
 This is a simple console-based snake game implemented in C++.
 [0] Start the game
 [1] Exit the game
 Please choose an option:)";
 
+struct Position {
+    int x;
+    int y;
+};
+
+class Snake {
+private:
+    int length;
+    queue<Position> body; // position of snake's body, snake's head in rear of queue, snake's rear in front of queue
+    int direction; // 0: up, 1: right, 2: down, 3: left
+
+public:
+    // TODO: 实现Snake类方法
+};
+
+class Food {
+private:
+    Position position; // position of food
+
+public:
+    // TODO: 实现Food类方法
+};
+
 int main()
 {
     // clear the console screen
-    std::system("clear || cls");
+    system("clear || cls");
 
     // set all output to green
-    std::cout << "\033[32m";
+    cout << "\033[32m";
 
     // print the banner and welcome message
-    std::cout << banner << "\n";
-    std::cout << welcomeMessage;
+    cout << banner << "\n";
+    cout << welcomeMessage;
 
     // wait for user input
     int choice;
-    std::cin >> choice;
+    cin >> choice;
 
     if (choice == 0)
     {
-        std::cout << "Starting the game...\n";
+        cout << "Starting the game...\n";
         // TODO: loop of game
     }
     else if (choice == 1)
     {
-        std::cout << "Exiting the game. Goodbye!\n";
+        cout << "Exiting the game. Goodbye!\n";
     }
     else
     {
-        std::cout << "Invalid choice. Please restart the game and choose a valid option.\n";
+        cout << "Invalid choice. Please restart the game and choose a valid option.\n";
     }
 
     return 0;
